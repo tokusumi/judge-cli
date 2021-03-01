a4b011c5847ed0d6262f355ba610e905eb80d691
tokusumi <tksmtoms@gmail.com>
2021-03-01 20:08:40 +0900

    Integrate testing with cli

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from typer.testing import CliRunner
 
 from judge import app
@@ -5,9 +6,14 @@ from judge import app
 runner = CliRunner()
 
 
+@pytest.mark.cli
+@pytest.mark.offline
 def test_app():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0, result.stdout
 
     result = runner.invoke(app, ["download", "--help"])
+    assert result.exit_code == 0, result.stdout
+
+    result = runner.invoke(app, ["test", "--help"])
     assert result.exit_code == 0, result.stdout
