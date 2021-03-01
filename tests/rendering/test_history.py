ff090abae0f1245ac8d49b200087b0629b3c0fad
tokusumi <tksmtoms@gmail.com>
2021-03-01 20:38:30 +0900

    Use tempfile

--- tests/rendering/test_history.py
+++ tests/rendering/test_history.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import tempfile
 
 from judge.rendering.history import Verbose
 from judge.rendering.history import render_history as rh
@@ -6,20 +7,30 @@ from judge.schema import History, JudgeStatus, TestCasePath
 
 
 def render_history(verbose: int) -> None:
-    for status in JudgeStatus:
-        history = History(
-            status,
-            TestCasePath("case 1", Path("temp.in"), Path("temp.out")),
-            output=b"123\n456\n",
-            exitcode=0,
-            elapsed=10,
-            memory=15,
-        )
-        _verbose: Verbose
-        for v in Verbose:
-            if verbose == v.value:
-                _verbose = v
-        rh(history, _verbose)
+    with tempfile.TemporaryDirectory() as _tempdir:
+        tempdir = Path(_tempdir) / "abc000_a"
+        tempdir.mkdir(parents=True, exist_ok=True)
+        temp_in = tempdir / "temp.in"
+        temp_out = tempdir / "temp.out"
+        with temp_in.open("wb") as f:
+            f.write(b"1 2 3")
+        with temp_out.open("wb") as f:
+            f.write(b"3164\n1111")
+
+        for status in JudgeStatus:
+            history = History(
+                status,
+                TestCasePath("case 1", temp_in, temp_out),
+                output=b"123\n456\n",
+                exitcode=0,
+                elapsed=10,
+                memory=15,
+            )
+            _verbose: Verbose
+            for v in Verbose:
+                if verbose == v.value:
+                    _verbose = v
+            rh(history, _verbose)
 
 
 def test_rendering_history():
