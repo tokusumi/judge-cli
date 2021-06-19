e69e7639f92cbde68c344dc4ab8666037cf296b9
tokusumi <tksmtoms@gmail.com>
2021-06-20 02:55:10 +0900

    add test

---
+++ tests/test_configure.py
@@ -0,0 +1,46 @@
+import tempfile
+
+import pytest
+import typer
+from typer.testing import CliRunner
+
+from judge.configure import main
+
+app = typer.Typer()
+app.command()(main)
+
+runner = CliRunner()
+
+
+def dummy():
+    return ""
+
+
+@pytest.mark.offline
+def test_conf(mocker):
+
+    mocker.patch("judge.tools.prompt.pydantic_prompt", return_value=dummy)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # success to create configuration file
+        result = runner.invoke(app, [tempdir])
+        assert result.exit_code == 0, result.stdout
+
+        # modify it
+        result = runner.invoke(app, [tempdir, "--problem", "a"])
+        assert result.exit_code == 0, result.stdout
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # success to create configuration file
+        result = runner.invoke(app, [tempdir, "-ii"])
+        assert result.exit_code == 0, result.stdout
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # success to create configuration file
+        result = runner.invoke(app, [tempdir, "-ip"])
+        assert result.exit_code == 0, result.stdout
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # success to create configuration file
+        result = runner.invoke(app, [tempdir, "-ia"])
+        assert result.exit_code == 0, result.stdout
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,9 @@ def test_app():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0, result.stdout
 
+    result = runner.invoke(app, ["conf", "--help"])
+    assert result.exit_code == 0, result.stdout
+
     result = runner.invoke(app, ["download", "--help"])
     assert result.exit_code == 0, result.stdout
 
