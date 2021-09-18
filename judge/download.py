fcee3f64c46f5e8f99891bd81f8968b8aad352f3
tokusumi <tksmtoms@gmail.com>
2021-09-19 01:44:25 +0900

    add use oj login ecosystem

--- judge/download.py
+++ judge/download.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Tuple
 
 import typer
 from onlinejudge import utils
@@ -7,7 +7,7 @@ from pydantic.networks import HttpUrl
 from pydantic.types import DirectoryPath
 
 from judge.schema import JudgeConfig
-from judge.tools.download import DownloadArgs, SaveArgs
+from judge.tools.download import DownloadArgs, LoginForm, SaveArgs
 from judge.tools.download import download as download_tool
 from judge.tools.download import save as save_tool
 
@@ -17,12 +17,20 @@ class DownloadJudgeConfig(JudgeConfig):
     testdir: DirectoryPath
 
 
+class CLILoginForm(LoginForm):
+    def get_credentials(self) -> Tuple[str, str]:
+        username = typer.prompt("What's your username?")
+        password = typer.prompt("What's your password?", hide_input=True)
+        return username, password
+
+
 def main(
     workdir: Path = typer.Argument(".", help="a directory path for working directory"),
     url: Optional[str] = typer.Option(None, help="a download URL"),
     directory: Path = typer.Option(None, help="a directory path for test cases"),
     no_store: bool = typer.Option(False, help="testcases is shown but not saved"),
     format: str = typer.Option("sample-%i.%e", help="custom filename format"),
+    login: bool = typer.Option(False, help="login into target service"),
     cookie: Path = typer.Option(utils.default_cookie_path, help="directory for cookie"),
 ) -> None:
     """
@@ -63,9 +71,13 @@ def main(
     typer.echo(f"Download {config.URL}")
 
     try:
+        login_form: Optional[LoginForm] = None
+        if login:
+            login_form = CLILoginForm()
         testcases = download_tool(
             DownloadArgs(
                 url=config.URL,
+                login_form=login_form,
                 cookie=cookie,
             )
         )
